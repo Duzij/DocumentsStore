0905a65b77e37bb5b6d31da306181089949e1f05
Maxim Duzij <maxim.duzij@hotmail.com>
2023-02-12 21:04:38 +0100

    Comments removed, production environment added

--- DocumentsStore/Program.cs
+++ DocumentsStore/Program.cs
@@ -4,31 +4,30 @@ using DocumentStore.BL.Stores;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMemoryCache();
 builder.Services.AddScoped<IDocumentRepository, InMemoryRepository>();
 builder.Services.AddScoped<IXmlConverter, XmlConverter>();
 builder.Services.AddScoped<IMessagePackConverter, MessagePackConverter>();
-builder.Services.AddScoped<IJsonConverter, DocumentStore.BL.Converters.JsonConverter>();
+builder.Services.AddScoped<IJsonConverter, JsonConverter>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionMiddleware();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 app.MapControllers();
-app.UseExceptionMiddleware();
 
 app.Run();
